78b48b23c74c0e8130ea3c3bb50848ab9b81ce23
jlmdev <jlmann72@gmail.com>
2024-10-21 11:12:03 -0400

    Initial Commit

---
+++ Resume/ResumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResumeApp.swift
+//  Resume
+//
+//  Created by JOSHUA MANN on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct ResumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ResumeTests/ResumeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ResumeTests.swift
+//  ResumeTests
+//
+//  Created by JOSHUA MANN on 10/21/24.
+//
+
+import Testing
+@testable import Resume
+
+struct ResumeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
