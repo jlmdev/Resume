26828fe6714a0bf830cd9a5912a0c15498a61b3d
jlmdev <jlmann72@gmail.com>
2024-10-23 10:54:42 -0400

    Rev 1.0

--- Resume/ContentView.swift
+++ Resume/ContentView.swift
@@ -9,22 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(
+            alignment: .leading,
+            spacing: 0)
+        {
+            
             HStack {
+                Text("Nick's Resume")
+                
                 Spacer()
+                
                 Image(.nickSquare)
                     .resizable()
                     .scaledToFit()
                     .frame(width: 100, height: 100)
-            }
-            Spacer()
-            
+                    .clipShape(Circle())
+                    .background {
+                        Circle()
+                            .offset(x: 1, y: 3)
+                            
+                            .blur(radius: 3)
+                    }
+            }//HStack
+            Group {
+                Text("January 1970 - Present").bold()
                 
-            Text("Hello, world!")
-        }
+                Rectangle()
+                    .frame(height: 1)
+                    .padding(.bottom, 10)
+                
+                Text("I didn't do anything at this time because I wasn't born yet. But I'm still learning and growing. \n\nI am learning swiftly, however.")
+                
+                
+                
+                Spacer()
+            }
+            
+            
+        }//VStack
         .padding()
-    }
-}
+    }//body
+}//view
 
 #Preview {
     ContentView()
